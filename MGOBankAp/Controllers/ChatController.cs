0d885809013599b3e589a8a6b764ce5a021190cb
Gasparyan Dav <zadref45@gmail.com>
2025-03-06 20:37:49 +0400

    Change chat front-end

--- MGOBankAp/Controllers/ChatController.cs
+++ MGOBankAp/Controllers/ChatController.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MGOBankApp.Controllers
 {
+    [Authorize]
     public class ChatController : Controller
     {
         private readonly ApplicationDbContext _context;
