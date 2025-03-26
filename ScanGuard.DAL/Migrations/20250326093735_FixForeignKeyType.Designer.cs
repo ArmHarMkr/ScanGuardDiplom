3248d21e314c657a0ba76edd9c0b4d7db0d1d753
Gasparyan Dav <zadref45@gmail.com>
2025-03-26 13:44:58 +0400

    Fix Corp migration

--- ScanGuard.DAL/Migrations/20250326093735_FixForeignKeyType.Designer.cs
+++ ScanGuard.DAL/Migrations/20250326093735_FixForeignKeyType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScanGuard.DAL.Data;
 
@@ -11,9 +12,11 @@ using ScanGuard.DAL.Data;
 namespace ScanGuard.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250326093735_FixForeignKeyType")]
+    partial class FixForeignKeyType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ScanGuard.DAL/Migrations/20250326093735_FixForeignKeyType.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScanGuard.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixForeignKeyType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Corporations",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CorpName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Website = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Zip = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WorkerCount = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AdminUserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    IsSubmitted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corporations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Corporations_AspNetUsers_AdminUserId",
+                        column: x => x.AdminUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CreateCorpRequests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CorporationId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RequestedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsApproved = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreateCorpRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CreateCorpRequests_Corporations_CorporationId",
+                        column: x => x.CorporationId,
+                        principalTable: "Corporations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Corporations_AdminUserId",
+                table: "Corporations",
+                column: "AdminUserId",
+                unique: true,
+                filter: "[AdminUserId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CreateCorpRequests_CorporationId",
+                table: "CreateCorpRequests",
+                column: "CorporationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CreateCorpRequests");
+
+            migrationBuilder.DropTable(
+                name: "Corporations");
+        }
+    }
+}
